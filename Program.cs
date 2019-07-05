636073d1606526f36fe2c2403c0f1c3445e4df28
gaganVirk <deep.virk@outlook.co.nz>
2019-07-05 15:13:31 +1200

    Time Complexity

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigO_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(2));
+        }
+        static int Sum(int n)
+        {
+            if(n <= 0)
+            {
+                return 0;
+            }
+
+            return n + Sum(n - 1);
+        }
+       
+    }
+}
